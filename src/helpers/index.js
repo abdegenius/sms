87c54ef79e358b4e333913236fae9acde5fbb74b
abdegenius <abijahjohnnie@gmail.com>
2021-07-30 12:54:24 +0100

    asdf

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,8 +3,9 @@ import { onMount } from "svelte";
 export const __serialize = JSON.stringify;
 // export const __deserialize = JSON.parse;
 
-export const __deserialize = (key) =>
+export const __deserialize = (key) => {
   JSON.parse(window.localStorage.getItem(key));
+}
 
 let ref = [];
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-export const SET_ZONE = writable([]);
+export const SET_ZONE = writable(0);
 export const SET_SCHOOLS = writable([]);
 export const CURRENT_SCHOOL = writable([]);
 export const SET_USER_TYPE = writable(null);
